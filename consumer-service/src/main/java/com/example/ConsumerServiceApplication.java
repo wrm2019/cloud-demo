eb364574225b982cb5f733b86f295804a752918a
wrm2019 <wang_ru_meng@sina.com>
2019-10-25 15:50:28 +0800

    hahhahhah

--- consumer-service/src/main/java/com/example/ConsumerServiceApplication.java
+++ consumer-service/src/main/java/com/example/ConsumerServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients //语序当前的项目使用feign声明的方式访问服务
 @EnableDiscoveryClient
+//这是注释
 public class ConsumerServiceApplication {
     public static void main(String[] args) {
 
